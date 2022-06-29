aecaf05e5d17f6776893ff6fff4e2bb2edd5c006
jrschmidt <jrschmidt56@gmail.com>
2022-06-29 12:42:49 -0700

    delete boilerplate demo components and add stripped down version of old TreeViewConsole component

---
+++ src/components/TreeViewConsole.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="tree-view-console">
+    <h6>TreeViewConsole component</h6>
+  </div>
+</template>
+
+<script setup>
+// <TreeViewWindow/>
+
+
+// import TreeViewWindow from './TreeViewWindow.vue'
+
+// import { provide } from 'vue'
+
+// import { familyTreeData } from '../family-tree-data.js'
+
+// provide ('familyTreeData', familyTreeData)
+</script>
+
+<style lang="css">
+.tree-view-console {
+  border-color: maroon;
+  border-style: dotted;
+}
+
+.tree-view-console h6 {
+  margin: 2px 0px 8px 8px;
+  text-align: left;
+  font-size: 0.6rem;
+  color: #999999;
+}
+</style>
