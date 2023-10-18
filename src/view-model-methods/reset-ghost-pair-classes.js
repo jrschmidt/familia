3baced2940a175a6e23811ae44553480da69d10c
jrschmidt <jrschmidt56@gmail.com>
2023-10-18 13:41:09 -0700

    added a call to copyPeopleIdsToGhosts() method to reset-ghost-pair-classes.js

--- src/view-model-methods/reset-ghost-pair-classes.js
+++ src/view-model-methods/reset-ghost-pair-classes.js
@@ -1,16 +1,21 @@
 // This method resets classes for 'ghost' pair components after shiftSet() and shiftMove().
 
 import { getPairShape } from "./get-pair-shape"
+import { copyPeopleIdsToGhosts } from './copy-people-ids-to-ghosts.js'
 
 import { pairLocationsList } from '../view-model-constants'
 
 export const resetGhostPairClasses = (viewModel) => {
 
-  // Reset location and shape classes to original value for ghost pair objects.
-   pairLocationsList.forEach( (loc) => {
+  pairLocationsList.forEach( (loc) => {
+
+    // Reset location and shape classes to original value for ghost pair objects.
     let pair = viewModel.peoplePairs.find( pp => pp.label === loc + '-ghost' )
     pair.classStatus.location = loc
     pair.classStatus.shape = getPairShape(loc)
+    
+    // Reset person id's for ghost pair objects to match person id's on corresponding
+    // static pair objects.
+    copyPeopleIdsToGhosts(viewModel)
   })
-
 }
